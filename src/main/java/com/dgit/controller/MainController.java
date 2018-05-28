398f98fa01f48b51edf1e8e96968bad202124663
HyeSeon Yang <skcm19@naver.com>
2018-05-28 17:57:33 +0900

    메인화면 Controller

---
+++ src/main/java/com/dgit/controller/MainController.java
@@ -0,0 +1,21 @@
+package com.dgit.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RequestMapping("/board/")
+@Controller
+public class MainController {
+
+	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+	
+	@RequestMapping(value="/mainPage", method=RequestMethod.GET)
+	public void listPageGet(){
+		logger.info("Board mainPageGet Get ......");
+		
+		
+	}
+}
