d3374ae6af3b81f7de008907488d1d762d014248
HyeSeon Yang <skcm19@naver.com>
2018-05-30 19:20:55 +0900

    login시 필요한 DTO 아이디와 이름

---
+++ src/main/java/com/dgit/domain/LoginDTO.java
@@ -0,0 +1,29 @@
+package com.dgit.domain;
+
+public class LoginDTO {
+	// 회원이 로그인 했나 안했나 확인하기 위해
+	private String u_id;
+	private String u_name;
+
+	public String getU_id() {
+		return u_id;
+	}
+
+	public void setU_id(String u_id) {
+		this.u_id = u_id;
+	}
+
+	public String getU_name() {
+		return u_name;
+	}
+
+	public void setU_name(String u_name) {
+		this.u_name = u_name;
+	}
+
+	@Override
+	public String toString() {
+		return "LoginDTO [u_id=" + u_id + ", u_name=" + u_name + "]";
+	}
+
+}
