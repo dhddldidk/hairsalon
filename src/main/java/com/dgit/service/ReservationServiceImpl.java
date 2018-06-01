2d92400297e0fe38be6af4ac0992b1a1d506c0e5
HyeSeon Yang <skcm19@naver.com>
2018-06-01 16:14:21 +0900

    예약된 리스트 Service Impl

---
+++ src/main/java/com/dgit/service/ReservationServiceImpl.java
@@ -0,0 +1,23 @@
+package com.dgit.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dgit.domain.ReservationVO;
+import com.dgit.persistence.ReservationDAO;
+
+@Service
+public class ReservationServiceImpl implements ReservationService {
+
+	@Autowired
+	ReservationDAO dao;
+	
+	@Override
+	public List<ReservationVO> selectAllReservation() throws Exception {
+		
+		return dao.selectAllReservation();
+	}
+
+}
