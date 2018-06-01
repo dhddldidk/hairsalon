0b4eb90d37b8e49d0ee521489ad2569175d71782
HyeSeon Yang <skcm19@naver.com>
2018-06-01 16:11:26 +0900

    헤어스타일 정보 controller

---
+++ src/main/java/com/dgit/controller/HairStyleController.java
@@ -0,0 +1,10 @@
+package com.dgit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/board")
+@Controller
+public class HairStyleController {
+
+}
