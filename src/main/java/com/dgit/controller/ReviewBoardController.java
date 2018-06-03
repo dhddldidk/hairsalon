baa8736167a102a7c0c576af26f1cf24a01a2799
HyeSeon Yang <skcm19@naver.com>
2018-06-03 21:58:54 +0900

    후기 게시글 리스트 test

--- src/main/java/com/dgit/controller/ReviewBoardController.java
+++ src/main/java/com/dgit/controller/ReviewBoardController.java
@@ -1,11 +1,13 @@
 package com.dgit.controller;
 
 import java.util.Date;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -26,9 +28,11 @@ public class ReviewBoardController {
 	
 	//게시판 리스트
 	@RequestMapping(value="/reviewListPage", method=RequestMethod.GET)
-	public void ReviewListPageGet(){
+	public void ReviewListPageGet(Model model) throws Exception{
 		logger.info("reviewListPage Get ......");
 		
+		List<ReviewBoardVO> boardList = service.selectAllReview();
+		model.addAttribute("boardList",boardList);
 		
 	}
 	
