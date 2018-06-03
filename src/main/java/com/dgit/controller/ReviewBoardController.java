fdf2699a7d6789b245744ef8d83fa654dcccdbf7
HyeSeon Yang <skcm19@naver.com>
2018-06-03 18:55:22 +0900

    후기 게시판 등록후 리스트 화면으로 전환

--- src/main/java/com/dgit/controller/ReviewBoardController.java
+++ src/main/java/com/dgit/controller/ReviewBoardController.java
@@ -1,16 +1,27 @@
 package com.dgit.controller;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.dgit.domain.ReviewBoardVO;
+import com.dgit.service.ReviewBoardService;
 
 @RequestMapping("/board/")
 @Controller
 public class ReviewBoardController {
 
 	private static final Logger logger = LoggerFactory.getLogger(ReviewBoardController.class);
+	
+	@Autowired
+	private ReviewBoardService service;
+	
+	
 	
 	
 	//게시판 리스트
@@ -30,9 +41,16 @@ public class ReviewBoardController {
 	}
 	
 	@RequestMapping(value="/reviewRegisterPage", method=RequestMethod.POST)
-	public void ReviewRegisterPagePost(){
-		logger.info("reviewRegisterPage GePost ......");
+	public String ReviewRegisterPagePost(ReviewBoardVO vo) throws Exception{
+		logger.info("reviewRegisterPage Post ......");
 		
+		logger.info(vo.toString());
 		
+		/*Date date = new Date();
+		vo.setRb_regdate(date);
+		logger.info("게시글 등록일"+vo.getRb_regdate());*/
+		service.insertReview(vo);
+		
+		return "redirect:/board/reviewListPage";
 	}
 }
