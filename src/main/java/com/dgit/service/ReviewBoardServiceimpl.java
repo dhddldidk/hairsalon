79358e3b97e2d8b688a2625b4c049fad77046601
HyeSeon Yang <skcm19@naver.com>
2018-06-03 18:54:53 +0900

    @Autowired DAO로 수정

--- src/main/java/com/dgit/service/ReviewBoardServiceimpl.java
+++ src/main/java/com/dgit/service/ReviewBoardServiceimpl.java
@@ -6,23 +6,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dgit.domain.ReviewBoardVO;
+import com.dgit.persistence.ReviewBoardDAO;
 
 @Service
 public class ReviewBoardServiceimpl implements ReviewBoardService {
 
 	@Autowired
-	ReviewBoardService service;
+	ReviewBoardDAO dao;
 	
 	@Override
 	public void insertReview(ReviewBoardVO vo) throws Exception {
-		service.insertReview(vo);
+		dao.insertReview(vo);
 
 	}
 
 	@Override
 	public List<ReviewBoardVO> selectAllReview() throws Exception {
 		
-		return service.selectAllReview();
+		return dao.selectAllReview();
 	}
 
 }
