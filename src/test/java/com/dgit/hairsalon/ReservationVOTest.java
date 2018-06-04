7624bb46a9437a18bf5a3fafb8a78ef1bb3d8ce8
HyeSeon Yang <skcm19@naver.com>
2018-06-04 23:26:11 +0900

    reservation insert test

--- src/test/java/com/dgit/hairsalon/ReservationVOTest.java
+++ src/test/java/com/dgit/hairsalon/ReservationVOTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.dgit.domain.HairStyleVO;
 import com.dgit.domain.ReservationVO;
 import com.dgit.persistence.ReservationDAO;
 
@@ -25,4 +26,14 @@ public class ReservationVOTest {
 			System.out.println(vo);
 		}
 	}
+	
+	/*@Test
+	public void test2insertReservation() throws Exception{
+		ReservationVO vo = new ReservationVO();
+		HairStyleVO hvo = new HairStyleVO();
+		
+		vo.setRes_no(10);
+		hvo.setHair_no(1);
+		
+	}*/
 }
