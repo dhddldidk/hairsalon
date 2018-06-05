2a096a72390ec5f8847a48644d72230117d60243
HyeSeon Yang <skcm19@naver.com>
2018-06-05 19:07:55 +0900

    마이페이지 예약 취소 Service

--- src/main/java/com/dgit/service/ReservationService.java
+++ src/main/java/com/dgit/service/ReservationService.java
@@ -9,4 +9,8 @@ public interface ReservationService {
 	public List<ReservationVO> selectAllReservation(String res_start, String res_end) throws Exception;
 	
 	public void insertReservation(ReservationVO vo) throws Exception;
+	
+	public List<ReservationVO> myPageReservation(String u_id) throws Exception;
+	
+	public void myPageDeleteReg(String u_id, int res_no) throws Exception;
 }
