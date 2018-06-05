055ca06ff906c88b92e082c7c2031309e36c725a
HyeSeon Yang <skcm19@naver.com>
2018-06-05 19:08:04 +0900

    마이페이지 예약 취소 Service

--- src/main/java/com/dgit/service/ReservationServiceImpl.java
+++ src/main/java/com/dgit/service/ReservationServiceImpl.java
@@ -1,6 +1,8 @@
 package com.dgit.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,6 +25,19 @@ public class ReservationServiceImpl implements ReservationService {
 	@Override
 	public void insertReservation(ReservationVO vo) throws Exception {
 		dao.insertReservation(vo);
+		
+	}
+
+	@Override
+	public List<ReservationVO> myPageReservation(String u_id) throws Exception {
+		
+		return dao.myPageReservation(u_id);
+	}
+
+	@Override
+	public void myPageDeleteReg(String u_id, int res_no) throws Exception {
+		
+		dao.myPageDeleteReg(u_id, res_no);
 		
 	}
 
