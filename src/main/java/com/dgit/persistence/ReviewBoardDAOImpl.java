4d538b339fe3c4386a182ce843a5acba92a57262
HyeSeon Yang <skcm19@naver.com>
2018-06-06 15:56:03 +0900

    후기 게시판 페이징 DAO

--- src/main/java/com/dgit/persistence/ReviewBoardDAOImpl.java
+++ src/main/java/com/dgit/persistence/ReviewBoardDAOImpl.java
@@ -6,7 +6,9 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.dgit.domain.Criteria;
 import com.dgit.domain.ReviewBoardVO;
+import com.dgit.domain.SearchCriteria;
 
 @Repository
 public class ReviewBoardDAOImpl implements ReviewBoardDAO {
@@ -43,6 +45,36 @@ public class ReviewBoardDAOImpl implements ReviewBoardDAO {
 	public void updateReview(ReviewBoardVO vo) throws Exception {
 		session.update(namespace+".updateReview",vo);
 		
+	}
+
+	@Override
+	public List<ReviewBoardVO> listPage(int page) throws Exception {
+		page = (page-1)*10;
+		return session.selectList(namespace+".listPage", page);
+	}
+
+	@Override
+	public List<ReviewBoardVO> listCriteria(Criteria cri) throws Exception {
+		
+		return session.selectList(namespace+".listCriteria", cri);
+	}
+
+	@Override
+	public List<ReviewBoardVO> listSearch(SearchCriteria cri) throws Exception {
+		
+		return session.selectList(namespace+".listSearch", cri);
+	}
+
+	@Override
+	public int totalCount() throws Exception {
+		
+		return session.selectOne(namespace+".totalCount");
+	}
+
+	@Override
+	public int totalSearchCount(SearchCriteria cri) throws Exception {
+		
+		return session.selectOne(namespace+".totalSearchCount"+cri);
 	}
 
 }
