f9f533635076e10b00a490db69f4e2a34bba148b
HyeSeon Yang <skcm19@naver.com>
2018-06-08 20:33:49 +0900

    코드 정리

--- src/main/java/com/dgit/controller/ReviewBoardController.java
+++ src/main/java/com/dgit/controller/ReviewBoardController.java
@@ -10,9 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.dgit.domain.Criteria;
 import com.dgit.domain.PageMaker;
 import com.dgit.domain.ReviewBoardVO;
 import com.dgit.domain.SearchCriteria;
