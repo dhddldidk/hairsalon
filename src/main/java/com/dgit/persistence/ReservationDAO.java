c12c109adcbc47435705f4e4d8bcd2418099ac88
HyeSeon Yang <skcm19@naver.com>
2018-06-11 16:03:06 +0900

    마이페이지 paging DAO

--- src/main/java/com/dgit/persistence/ReservationDAO.java
+++ src/main/java/com/dgit/persistence/ReservationDAO.java
@@ -2,6 +2,7 @@ package com.dgit.persistence;
 
 import java.util.List;
 
+import com.dgit.domain.Criteria;
 import com.dgit.domain.ReservationVO;
 
 public interface ReservationDAO {
@@ -10,7 +11,12 @@ public interface ReservationDAO {
 	
 	public void insertReservation(ReservationVO vo) throws Exception;
 	
+	//예약내역
 	public List<ReservationVO> myPageReservation(String u_id) throws Exception;
+	
+	//예약내역 페이징
+	public List<ReservationVO> myPageListCriteria(String u_id, Criteria cri) throws Exception;
+	public int myPageTotalCount(String u_id, Criteria cri) throws Exception;
 	
 	public void myPageDeleteReg(String u_id, int res_no) throws Exception;
 }
