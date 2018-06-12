4e8701e00d4e953e0d315f6abcef5237b82e2317
HyeSeon Yang <skcm19@naver.com>
2018-06-12 16:29:11 +0900

    Reply댓글 insert, list Service

---
+++ src/main/java/com/dgit/service/ReplyService.java
@@ -0,0 +1,15 @@
+package com.dgit.service;
+
+import java.util.List;
+
+import com.dgit.domain.Criteria;
+import com.dgit.domain.ReplyVO;
+
+public interface ReplyService {
+
+	public List<ReplyVO> listReply(int rb_no, Criteria cri) throws Exception;
+	
+	public int replyTotalCount(int rb_no) throws Exception;
+	
+	public void insertReply(ReplyVO vo) throws Exception;
+}
