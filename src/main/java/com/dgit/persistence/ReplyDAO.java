7fe5b741457d020a5544bfee249c82c334bb1844
HyeSeon Yang <skcm19@naver.com>
2018-06-12 17:26:52 +0900

    후기게시판 댓글 삭제 DAO

--- src/main/java/com/dgit/persistence/ReplyDAO.java
+++ src/main/java/com/dgit/persistence/ReplyDAO.java
@@ -7,10 +7,14 @@ import com.dgit.domain.ReplyVO;
 
 public interface ReplyDAO {
 
+	//댓글 페이징
 	public List<ReplyVO> listReply(int rb_no, Criteria cri) throws Exception;
 	
+	//댓글 총 개수
 	public int replyTotalCount(int rb_no) throws Exception;
 	
 	public void insertReply(ReplyVO vo) throws Exception;
+	
+	public void deleteReply(int reply_no) throws Exception;
 	
 }
