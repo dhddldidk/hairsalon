b617e66aa47cf4fc15f7a57c9c324b1452d30d10
HyeSeon Yang <skcm19@naver.com>
2018-06-13 22:33:00 +0900

    헤어스타일 리스트, 등록 Controller

--- src/main/java/com/dgit/controller/HairStyleController.java
+++ src/main/java/com/dgit/controller/HairStyleController.java
@@ -1,10 +1,28 @@
 package com.dgit.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@RequestMapping("/board")
+@RequestMapping("/hairstyle")
 @Controller
 public class HairStyleController {
 
+	private static final Logger logger = LoggerFactory.getLogger(HairStyleController.class);
+	
+	@RequestMapping(value="/hairstyleList", method=RequestMethod.GET)
+	public void hairstyleListGet(){
+		logger.info("hairstyleList Get ......");
+		
+		
+	}
+	
+	@RequestMapping(value="/hairstyleRegister", method=RequestMethod.GET)
+	public void hairstyleRegisterGet(){
+		logger.info("hairstyle Get ......");
+		
+		
+	}
 }
