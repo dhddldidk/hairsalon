c585d4ee0ce809c1217afcb084d6ca2d81ab1cc2
HyeSeon Yang <skcm19@naver.com>
2018-06-14 12:45:06 +0900

    HairStyle 모든정보 list(+헤어이미지) Service

--- src/main/java/com/dgit/service/HairStyleService.java
+++ src/main/java/com/dgit/service/HairStyleService.java
@@ -7,4 +7,10 @@ import com.dgit.domain.HairStyleVO;
 public interface HairStyleService {
 
 	public List<HairStyleVO> selectHairStyle() throws Exception;
+	
+	//헤어스타일 등록
+	public void insertHairStyle(HairStyleVO vo) throws Exception;
+	
+	//헤어스타일 모든 정보
+	public List<HairStyleVO> selectAllHairInfo() throws Exception;
 }
