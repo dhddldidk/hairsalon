fac9c2d2abff4122cdae42b7f2eb4090ccf794da
HyeSeon Yang <skcm19@naver.com>
2018-06-14 12:45:33 +0900

    HairStyle 모든정보 list(+헤어이미지) DAO

--- src/main/java/com/dgit/persistence/HairStyleDAO.java
+++ src/main/java/com/dgit/persistence/HairStyleDAO.java
@@ -6,5 +6,13 @@ import com.dgit.domain.HairStyleVO;
 
 public interface HairStyleDAO {
 
+	//예약페이지 select박스
 	public List<HairStyleVO> selectHairStyle() throws Exception;
+	
+	//헤어스타일 등록
+	public void insertHairStyle(HairStyleVO vo) throws Exception;
+	public void addHairAttach(String hair_filename) throws Exception;
+	
+	//헤어스타일 모든 정보
+	public List<HairStyleVO> selectAllHairInfo() throws Exception;
 }
