59ae8a6e9bfb76469a78e431d2840b84aaebf5d2
HyeSeon Yang <skcm19@naver.com>
2018-06-14 12:45:00 +0900

    HairStyle 모든정보 list(+헤어이미지) Service

--- src/main/java/com/dgit/service/HairStyleServiceImpl.java
+++ src/main/java/com/dgit/service/HairStyleServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dgit.domain.HairStyleVO;
 import com.dgit.persistence.HairStyleDAO;
@@ -19,5 +20,26 @@ public class HairStyleServiceImpl implements HairStyleService {
 		
 		return dao.selectHairStyle();
 	}
+
+	@Transactional
+	@Override
+	public void insertHairStyle(HairStyleVO vo) throws Exception {
+		dao.insertHairStyle(vo);
+		
+		if(vo.gethFiles()==null){
+			return;
+		}
+		for(String hair_filename : vo.gethFiles()){
+			dao.addHairAttach(hair_filename);
+		}
+	}
+
+	@Override
+	public List<HairStyleVO> selectAllHairInfo() throws Exception {
+		
+		return dao.selectAllHairInfo();
+	}
+
+	
 
 }
