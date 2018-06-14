a434db2e170d0a1e630deead84253f9095718b06
HyeSeon Yang <skcm19@naver.com>
2018-06-14 17:11:06 +0900

    헤어스타일 수정 Controller

--- src/main/java/com/dgit/service/HairStyleService.java
+++ src/main/java/com/dgit/service/HairStyleService.java
@@ -13,4 +13,13 @@ public interface HairStyleService {
 	
 	//헤어스타일 모든 정보
 	public List<HairStyleVO> selectAllHairInfo() throws Exception;
+	
+	//헤어스타일 read
+	public HairStyleVO readHairStyle(int hair_no) throws Exception;
+	
+	//헤어스타일 삭제
+	public void deleteHairStyle(int hair_no) throws Exception;
+	
+	//헤어스타일 수정
+	public void updateHairStyle(HairStyleVO vo, String[] oldFiles) throws Exception;
 }
