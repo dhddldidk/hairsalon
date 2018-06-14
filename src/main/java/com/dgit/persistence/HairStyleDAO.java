705a7899afa3374a0ae8f1f15740c96461c06018
HyeSeon Yang <skcm19@naver.com>
2018-06-14 17:11:26 +0900

    헤어스타일 수정 DAO

--- src/main/java/com/dgit/persistence/HairStyleDAO.java
+++ src/main/java/com/dgit/persistence/HairStyleDAO.java
@@ -15,4 +15,16 @@ public interface HairStyleDAO {
 	
 	//헤어스타일 모든 정보
 	public List<HairStyleVO> selectAllHairInfo() throws Exception;
+	
+	//헤어스타일 read
+	public HairStyleVO readHairStyle(int hair_no) throws Exception;
+	public List<String> getHairAttach(int hair_no) throws Exception;
+	
+	//헤어스타일 삭제
+	public void deleteHairStyle(int hair_no) throws Exception;
+	public void deleteHairAttach(int hair_no, String hair_filename) throws Exception;
+	
+	//헤어스타일 수정
+	public void updateHairStyle(HairStyleVO vo) throws Exception;
+	public void replaceHairAttach(String hair_filename, int hair_no) throws Exception;
 }
