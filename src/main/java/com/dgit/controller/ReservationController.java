07de1f8510372646eefd7c07546b828969f7bfb8
HyeSeon Yang <skcm19@naver.com>
2018-06-20 17:18:00 +0900

    관리자 로그인 시 예약화면 고객명,연락처 추가 Controller

--- src/main/java/com/dgit/controller/ReservationController.java
+++ src/main/java/com/dgit/controller/ReservationController.java
@@ -111,5 +111,55 @@ public class ReservationController {
 		return entity;
 	}
 	
+	/*------------------------------- 관리자 로그인 시 -------------------------------------- */
+	@RequestMapping(value="/reservationAdmin", method=RequestMethod.GET)
+	public void reservationAdminGet(Model model, HttpServletRequest request) throws Exception{
+		logger.info("reservation Get ......");
+		
+		HttpSession session = request.getSession();
+		LoginDTO loginDTO = (LoginDTO)session.getAttribute("login");
+		
+		//select 박스
+		List<HairStyleVO> hairList = hairService.selectHairStyle();
+		
+		for(HairStyleVO vo : hairList){
+			logger.info("db에서 가져온 헤어시간"+vo.getHair_time());
+		}
+		
+		model.addAttribute("hairList",hairList);
+
+	}
 	
+	//예약된 리스트들 가져와서 화면에 뿌려줌
+		@ResponseBody
+		@RequestMapping(value="/resAllAdmin", method=RequestMethod.GET)
+		public ResponseEntity<List<ReservationVO>> resAllAdmin(String res_start, String res_end) throws Exception{
+			logger.info("resAll Get ......");
+			
+			ResponseEntity<List<ReservationVO>> entity = null;
+			
+			logger.info("달력달력 ......"+res_start);
+			logger.info("달력달력 ......"+res_end);
+		
+			//예약된 모든 리스트
+			List<ReservationVO> resList = resService.selectAllReservationAdmin(res_start, res_end);
+			for(ReservationVO res : resList){
+				res.setRes_start(res.getRes_start());
+				res.setRes_end(res.getRes_end());
+				logger.info("예약자 이름 ......"+res.getMember().getU_name());
+				logger.info("예약자 연락처 ......"+res.getMember().getU_phone());
+				logger.info("예약된 리스트"+res);
+			}
+			
+			try {
+				List<ReservationVO> result = resService.selectAllReservationAdmin(res_start, res_end);
+				
+				entity = new ResponseEntity<>(result, HttpStatus.OK);
+			} catch (Exception e) {
+				e.printStackTrace();
+				entity = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+			}
+			
+			return entity;
+		}
 }
